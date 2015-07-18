3a716e700457d7e0a67c9878bd3cd2e42357319e
Kuraara Forrest <kuraara.forrest@gmail.com>
2015-07-18 21:34:46 +0800

    Got it working pretty well, and pretty quickly because I have no life, no job and I'm bored out of my mind. Woooo

--- game.js
+++ game.js
@@ -1,116 +1,212 @@
+/* global Handlebars */
 $(function(){
     
     var width = 10;
     var height = 10;
     
-    function getID(X,Y){
-        return ("#"+Y) + X;
+    function createCells(){
+        var cells = new Array(width*height);
+    
+        for(var i = 0; i < height; i++){
+            for(var j =0; j < width; j++){
+                var id = (i*width) + j; 
+                cells[ id ] = {ID:("#"+i)+j, y:i, x:j, xpos:(j*24), ypos:(i*24)};
+            }
+        }
+        
+        return cells;
     }
     
+    // generate random X/Y values
+    function randX(){
+        return  Math.floor((Math.random() * 10)) % width;
+    }
+    
+    function randY(){
+        return  Math.floor((Math.random() * 10)) % height;
+    }
+    
+    // Add a bomb to a random spot on the board
+    function addRand(b)
+    {
+        var success = false;
+        
+        var x = randX();
+        var y = randY();
+        
+        while(!success){
+            if(b[y][x] == 0)
+            {
+                success = true;
+                
+            }
+            else{
+                x = randX();
+                y = randY();
+            }
+        }
+        b[y][x] = 1;
+    }
+    
+    function getID(X,Y){
+        var i = (Y*width)+X;
+        var h = "#";
+        if(i < 10){
+            h = h + "0" + i;
+        }
+        else{
+            h+=i;
+        }
+        return h;
+    }
+       
+    var textCols = ["n1","n2","n3","n4","n5","n6","n7","n8"];
+    
+    var brd = setupBrd(12);
+    var data = {cells: createCells()};
+    var source = $("#cell-template").html();
+    var template = Handlebars.compile(source);
+    $(".board").append(template(data));
     function setIDs(){
         $(".cell").each(function(i,v){
-            console.log(i);
-            $(v).attr("id", i);
+            if(i < 10){
+                $(v).attr("id", "0"+i);    
+            }
+            else{
+                $(v).attr("id", i);
+            }
         });
     }
     
-    setIDs();
+    setIDs();     
+       
+    function xRnge(X,Y){
+        
+        var l = X;
+        var r = X;
+        
+        while(l > 0){
+            if(countNbrs(l,Y) == 0){
+                l--;
+            }
+            else
+            {
+                break;
+            }
+        }
+        while(r < width-1){
+            if(countNbrs(r,Y) == 0){
+                r++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return [l,r];
+    }
     
-    function setupBrd(){
+    function yRnge(X,Y){
+        
+        var u = Y;
+        var d = Y;
+        
+        while(u >= 0){
+            if(countNbrs(X,u) == 0 ){
+                u--;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        while(d < height-1){
+            if(countNbrs(X,d) == 0){
+                d++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return [u,d];
+    }
+    
+    function makeBlank(X,Y){
+        var lr = xRnge(X,Y);
+        
+        for(var i = lr[0]; i < lr[1]+1; i++){
+            var yr = yRnge(i, Y)
+            
+            for(var j = yr[0]; j < yr[1]+1;j++){
+                var id = getID(i,j);
+                
+                $(id).removeClass("unpressed");
+                $(id).addClass("pressed");
+            }
+        }
+    }
+    
+
+        
+    function press(e){
+        if($(e).hasClass("unpressed")){
+            $(e).removeClass("unpressed");
+            $(e).addClass("pressed");
+            var x = $(e).data("x");
+            var y = $(e).data("y");
+            var nbs = countNbrs(x,y);
+            if(brd[y][x] == 0){
+                if( nbs > 0 ){
+                    $(e).html(nbs);
+                    $(e).addClass(textCols[nbs-1]);
+                }
+                else{
+                    makeBlank(x,y);  
+                }
+            }
+            else{
+                $(e).html("X");
+                $(e).addClass("derp");
+                gameOver();
+            }
+            
+        }
+    }
+    
+    function setupBrd(n){
         var b = new Array(10);
         for(var i = 0; i < 10; i++){
             b[i] = [0,0,0,0,0,0,0,0,0,0];
         }
         
-        b[0][1] = 1;
-        b[0][0] = 1;
-        b[0][4] = 1;
-        b[0][7] = 1;
-        b[3][1] = 1;
-        b[4][2] = 1;
-        b[5][3] = 1;
-        b[5][5] = 1;
-        
+        for(var i = 0; i < n; i++){
+            addRand(b);
+        }
         
         return b;
     }
     
-    var textCols = ["n1","n2","n3","n4","n5"];
+
     
-    var brd = setupBrd();
-    
-    function fill(X,Y){
+    function gameOver(){
         
-        if(brd[Y][X] == 0){
-            var id = getID(X,Y);
-            $(id).removeClass("unpressed");
-            $(id).addClass("pressed");
-            
-            var L = X-1;
-            var R = X;
-            var U = Y;
-            var D = Y+1;
-            
-            for(var l = L; l >= 0; l--){
-                for(var u = U; u >= 0; u--){
-                    if(brd[u][l] == 0 && countNbrs(l,u) == 0 )
-                    {
-                        id = getID(l,u);
-                        $(id).removeClass("unpressed");
-                        $(id).addClass("pressed");
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }// uo
-                
-                for(var d = D; d < height; d++){
-                    if(brd[d][l] == 0 && countNbrs(l,d) == 0 )
-                    {
-                        id = getID(l,d);
-                        $(id).removeClass("unpressed");
-                        $(id).addClass("pressed");
-                    }
-                    else
-                    {
-                        break;
-                    }
+        for(var i = 0; i < height; i++){
+            for(j = 0; j < width; j++){
+                if(brd[i][j] == 1){
+                    console.log("j: ", i, " j: ", j);
+                    var b = $(getID(j,i));
+                    $(b).addClass("pressed");
+                    $(b).html("X");
                 }
-            } // left
-            
-            for(var l = R; l < height; l++){
-                for(var u = U; u >= 0; u--){
-                    if(brd[u][l] == 0 && countNbrs(l,u) == 0 )
-                    {
-                        id = getID(l,u);
-                        $(id).removeClass("unpressed");
-                        $(id).addClass("pressed");
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }// uo
-                
-                for(var d = D; d < height; d++){
-                    if(brd[d][l] == 0 && countNbrs(l,d) == 0 )
-                    {
-                        id = getID(l,d);
-                        $(id).removeClass("unpressed");
-                        $(id).addClass("pressed");
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-            } // right
-                        
-            
+            }
         }
+        
+        alert("Game over.");
+        resetGame();
+        
     }
-    
     
     function countNbrs(X,Y){
         var u = Y-1;
@@ -148,39 +244,23 @@ $(function(){
         
     }
     
-    console.log($(brd));
-    
     $(".cell").click(function(){
-            if($(this).hasClass("unpressed")){
-                $(this).removeClass("unpressed");
-                $(this).addClass("pressed");
-            }
-            
-            var x = $(this).data('x');
-            var y = $(this).data('y');
-            
-            var id = getID(x,y);
-            
-            var v = brd[y][x];
-            console.log("id: "+id+ " x: " + x + " y: " + y + " val: " + v);
-            
-            if(v == 0){
-                var nbs = countNbrs(x,y);
-                if(nbs > 0){
-                    $(this).html(nbs);
-                    $(this).addClass(textCols[nbs-1]);
-                }
-                else
-                {
-                    fill(x,y);
-                }
-            }
-            else{
-                $(this).html("X");
-            }
+            press($(this));
             
         }
     );    
     
+    function resetGame(){
+        brd = setupBrd(12)
+        for(var i = 0; i < height; i++){
+            for(j = 0; j < width; j++){
+                var b = $(getID(j,i));
+                $(b).removeClass("pressed");
+                $(b).removeClass("derp");  
+                $(b).addClass("unpressed");
+                $(b).empty();
+            }
+        }
+    }
     
 });
